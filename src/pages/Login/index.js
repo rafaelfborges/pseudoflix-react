15cf06edb6a8ce3a3c878d7970f17b9bcfd41f42
Rafael Borges <rafaelfborges@gmail.com>
2020-11-06 20:51:40 -0300

    Fix login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,38 +1,32 @@
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
+
 import "./Login.css";
 
 function Login() {
   return (
-    <div style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-    }}>
-        <Container>
-            <Row>
-                <Col>
-                    <Form className="form-login" >
-                        <Form.Group controlId="formBasicEmail" >
-                            <Form.Label>E-mail</Form.Label>
-                            <Form.Control type="email" placeholder="E-mail" />
-                            <Form.Text className="text-muted">
-                            </Form.Text>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Senha</Form.Label>
-                            <Form.Control type="password" placeholder="Senha" />
-                        </Form.Group>
-                        <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Lembrar E-mail" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Form className="form-login">
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control type="email" placeholder="E-mail" />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control type="password" placeholder="Senha" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Lembrar E-mail" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 

b10959c186bd0ee9a1b34e948ed176ca85b33c92
Rafael Borges <rafaelfborges@gmail.com>
2020-11-06 20:34:25 -0300

    Add user register page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//import NavBar from "components/NavBar";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,15 +11,17 @@ import Page404 from "./pages/404";
 
 export default function Routes() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/admin" component={Admin} />
-        <Route path="*" component={Page404} />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/admin" component={Admin} />
+          <Route path="*" component={Page404} />
+        </Switch>
+      </Router>
+    </>
   );
 }

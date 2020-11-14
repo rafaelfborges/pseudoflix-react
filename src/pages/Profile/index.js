667434b9e771cd047f46f50da3c3168d9fdf05a9
Carlos Eduardo Goulart de Almeida <dudu020300@gmail.com>
2020-11-13 22:21:04 -0300

    Alterado CSS, tratativas de layout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,43 +1,61 @@
 import NavBar from "../../components/NavBar";
 import {useState} from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Form, Container, Row, Col } from "react-bootstrap";
 
 
 function Profile() {
-    const [nome, setNome] = useState("Carlos");
-    const [dataNasc, setDataNasc] = useState("02/11/2200");
-    const [email, setEmail] = useState("carlos@gmail.com");
+    const [nome] = useState("Carlos");
+    const [dataNasc] = useState("02/11/2200");
+    const [email] = useState("carlos@gmail.com");
 
   return (
     <>
       <NavBar/>
-      <div className='container'>
-          <div className='row'>
-              <div className='col-sm-12 text-center'>
+      <Container className='container-fluid w-100'>
+          <Row>
+              <Col className='text-center'>
                   <h1>Perfil do Usuário</h1>
-              </div>
-          </div>
-          <div className='row'>
-              <div className='col-sm-4'>
-                  <p>Nome              </p>
-                  <p>Email             </p>
-                  <p>Data de Nascimento</p>
-                  <p><label htmlFor='formAvatar'>Insira a imagem do seu avatar</label></p>
-              </div>
-              <div className='col-sm-8 font-weight-bold'>
-                  <p><span >{nome}</span></p>
-                  <p><span>{email}</span></p>
-                  <p><span>{dataNasc}</span></p>
-                  <p>
-                      <Form>
-                          <Form.Group>
-                              <Form.File id="formAvatar"/>
-                          </Form.Group>
-                      </Form>
-                  </p>
-              </div>
-          </div>
-      </div>
+              </Col>
+          </Row>
+          <Row >
+              <Col xs='12' sm='12' md='6' lg='6' className='align-content-xl-start.'>
+                  <Row >
+                      <Col xs='5' sm='5' md='4' lg='4' className='font-weight-bold'>Nome:</Col>
+                      <Col xs='7' sm='7' md='2' lg='2' className='overflow-auto'>{nome}</Col>
+                  </Row>
+              </Col>
+              <Col xs='12' sm='12' md='6' lg='6'>
+                  <Row>
+                      <Col xs='5' sm='5' md='4' lg='4' className='font-weight-bold'>Email:</Col>
+                      <Col xs='7' sm='7' md='2' lg='2'>{email}</Col>
+                  </Row>
+              </Col>
+              <Col sm='12' md='12' lg='12'>
+                  <hr/>
+              </Col>
+              <Col xs='12'sm='12' md='12' lg='12'>
+                  <Row>
+                      <Col xs='5' sm='5' md='6' lg='4' className='font-weight-bold'>Data de Nascimento:</Col>
+                      <Col xs='7' sm='7' md='6' lg='8'>{dataNasc}</Col>
+                  </Row>
+              </Col>
+          </Row>
+          <hr/>
+          <Row>
+              <Col xs='12' sm='12' md='12' lg='12' >
+                  <Row>
+                      <Col xs='5' sm='5' md='6' lg='6' className='font-weight-bold'>Inserir Arquivo de Perfil:</Col>
+                      <Col xs='7' sm='7' md='6' lg='6' className='overflow-auto'>
+                          <Form>
+                              <Form.Group>
+                                  <Form.File id="formAvatar"/>
+                              </Form.Group>
+                          </Form>
+                      </Col>
+                  </Row>
+              </Col>
+          </Row>
+      </Container>
 
     </>
   );
